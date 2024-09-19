41aa783a4529d398cc19399b57370f2d1b6206f5
junhkang <junhkang91@gmail.com>
2024-09-19 13:24:30 +0900

    initial commit

---
+++ src/main/java/io/github/junhkang/springboottesting/SpringBootTestingFromZeroToHeroApplication.java
@@ -0,0 +1,13 @@
+package io.github.junhkang.springboottesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingFromZeroToHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingFromZeroToHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/junhkang/springboottesting/SpringBootTestingFromZeroToHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.junhkang.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingFromZeroToHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
